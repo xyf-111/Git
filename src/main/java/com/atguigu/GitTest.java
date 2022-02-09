a1ac161b04788c6c513ee63cfd7dad51208c0ec8
xyf <xyf@126.com>
2022-02-09 21:29:51 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+
+/**
+ * @author xyf
+ * @create 2022-02-09 21:20
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
